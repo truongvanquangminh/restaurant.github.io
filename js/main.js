6079c69c54073e2fdd83c3ea1e46fa4cf10ac916
truongvanquangminh <minhtruong20002@gmail.com>
2021-01-14 21:08:55 +0700

    Edit

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ $(document).ready(function(){
 
   $(".owl-carousel").owlCarousel({
     loop:true,
-    nav:true,
+    nav:false,
     items: 1,
     dots: true,
-
+    autoplay: true,
+    autoplatTimeout:2000
   });
 });
